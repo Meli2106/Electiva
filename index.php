b213c90213974f528833a128003a9d8d6aa62280
Meli2106 <melizza2106sv@hotmail.com>
2022-05-06 19:28:06 -0500

    Na

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include ("template/cabecera.php");?> <!--menucabecera-->
 <?php include ("conexion.php");?> 
-
+ <?php echo " Hola "?>
 
 <?php include ("template/footer.php");?><!--footer-->
 
